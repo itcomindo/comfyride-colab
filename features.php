db0c2b08748fe075b2a99b07c44e7aafc726074f
Keanu <keanuryansyah1204@gmail.com>
2023-06-23 02:09:19 +0700

    kenu

--- features.php
+++ features.php
@@ -1,5 +1,49 @@
-<section id="featureSec" class="feature">
+<section id="featureSec" class="section">
     <div id="featureCtn" class="container">
-        
+        <div class="featureTitle">
+            <h3>WE DO THE BEST</h3>
+            <h2>THAN YOU WISH</h2>
+        </div>
+        <div class="featureBoxWr">
+            <div class="featureBoxChild">
+                <div class="iconFeature">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M316.9 18C311.6 7 300.4 0 288.1 0s-23.4 7-28.8 18L195 150.3 51.4 171.5c-12 1.8-22 10.2-25.7 21.7s-.7 24.2 7.9 32.7L137.8 329 113.2 474.7c-2 12 3 24.2 12.9 31.3s23 8 33.8 2.3l128.3-68.5 128.3 68.5c10.8 5.7 23.9 4.9 33.8-2.3s14.9-19.3 12.9-31.3L438.5 329 542.7 225.9c8.6-8.5 11.7-21.2 7.9-32.7s-13.7-19.9-25.7-21.7L381.2 150.3 316.9 18z"/></svg></i>
+                </div>
+                <div class="featureText">
+                    <h3>PICKUP</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, perferendis eos. Cupiditate incidunt animi esse?</p>
+                </div>
+            </div>
+            <div class="featureBoxChild">
+                <div class="iconFeature">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M190.5 68.8L225.3 128H224 152c-22.1 0-40-17.9-40-40s17.9-40 40-40h2.2c14.9 0 28.8 7.9 36.3 20.8zM64 88c0 14.4 3.5 28 9.6 40H32c-17.7 0-32 14.3-32 32v64c0 17.7 14.3 32 32 32H480c17.7 0 32-14.3 32-32V160c0-17.7-14.3-32-32-32H438.4c6.1-12 9.6-25.6 9.6-40c0-48.6-39.4-88-88-88h-2.2c-31.9 0-61.5 16.9-77.7 44.4L256 85.5l-24.1-41C215.7 16.9 186.1 0 154.2 0H152C103.4 0 64 39.4 64 88zm336 0c0 22.1-17.9 40-40 40H288h-1.3l34.8-59.2C329.1 55.9 342.9 48 357.8 48H360c22.1 0 40 17.9 40 40zM32 288V464c0 26.5 21.5 48 48 48H224V288H32zM288 512H432c26.5 0 48-21.5 48-48V288H288V512z"/></svg></i>
+                </div>
+                <div class="featureText">
+                    <h3>BONUSES RIDE</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, perferendis eos. Cupiditate incidunt animi esse?</p>
+                </div>
+            </div>
+            <div class="featureBoxChild">
+                <div class="iconFeature">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M0 24C0 10.7 10.7 0 24 0H360c13.3 0 24 10.7 24 24s-10.7 24-24 24h-8V67c0 40.3-16 79-44.5 107.5L225.9 256l81.5 81.5C336 366 352 404.7 352 445v19h8c13.3 0 24 10.7 24 24s-10.7 24-24 24H24c-13.3 0-24-10.7-24-24s10.7-24 24-24h8V445c0-40.3 16-79 44.5-107.5L158.1 256 76.5 174.5C48 146 32 107.3 32 67V48H24C10.7 48 0 37.3 0 24zM110.5 371.5c-3.9 3.9-7.5 8.1-10.7 12.5H284.2c-3.2-4.4-6.8-8.6-10.7-12.5L192 289.9l-81.5 81.5zM284.2 128C297 110.4 304 89 304 67V48H80V67c0 22.1 7 43.4 19.8 61H284.2z"/></svg></i>
+                </div>
+                <div class="featureText">
+                    <h3>FAST BOOKING</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, perferendis eos. Cupiditate incidunt animi esse?</p>
+                </div>
+            </div>
+            <div class="featureBoxChild">
+                <div class="iconFeature">
+                    <i><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg></i>
+                </div>
+                <div class="featureText">
+                    <h3>GPS SEARCHING</h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, perferendis eos. Cupiditate incidunt animi esse?</p>
+                </div>
+            </div>
+        </div>
+        <div class="buttonWr">
+            <a href="">ini button</a>
+        </div>
     </div>
 </section>
